f597ce202d5275c2218e6d5f4e6d42bda6b7ab94
Ethan Xue <ethan.xue@gmail.com>
2018-02-09 00:27:22 -0500

    load data from first folder

--- src/load_data.py
+++ src/load_data.py
@@ -5,5 +5,15 @@ def load_data():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     file_path = os.path.abspath(os.path.join(dir_path, '..', 'raw_data', '250hardnospam'))
 
-    file = open(os.path.join(file_path, os.listdir(file_path)[40]))
-    print(file.read())
+    raw_training_set = [] 
+
+    for file in os.listdir(file_path):
+        if file == '.DS_Store':
+            continue
+        file = open(os.path.join(file_path, file))
+        raw_training_set.append({
+            'contents': file.read(),
+            'is_spam': True,
+        })
+
+    print(raw_training_set[4].get('contents'))
