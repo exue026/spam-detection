51898c6ff3eb64131cc4d31c200d87c7211b1361
Ethan Xue <ethan.xue@gmail.com>
2018-02-10 09:49:08 -0500

    side project spam

--- src/load_data.py
+++ src/load_data.py
@@ -1,19 +1,25 @@
 import os
-import glob
 
 def load_data():
     dir_path = os.path.dirname(os.path.realpath(__file__))
-    file_path = os.path.abspath(os.path.join(dir_path, '..', 'raw_data', '250hardnospam'))
+    file_path = os.path.abspath(os.path.join(dir_path, '..', 'raw_data'))
 
-    raw_training_set = [] 
-
-    for file in os.listdir(file_path):
-        if file == '.DS_Store':
+    directories = os.listdir(file_path)
+    for directory in directories:
+        if not directory == '250hardnospam':
             continue
-        file = open(os.path.join(file_path, file))
-        raw_training_set.append({
-            'contents': file.read(),
-            'is_spam': True,
-        })
-
+        is_spam = True
+        if 'nospam' in directory:
+            is_spam = False
+        
+        raw_training_set = [] 
+        dir_path = os.path.join(file_path, directory) 
+        for file in os.listdir(dir_path):
+            if file == '.DS_Store':
+                continue
+            file = open(os.path.join(dir_path, file))
+            raw_training_set.append({
+                'contents': file.read(),
+                'is_spam': is_spam,
+            })
     return raw_training_set
