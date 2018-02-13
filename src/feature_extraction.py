b072ae6fe1632b271996d40bbbc0122d00ea862b
Ethan Xue <ethan.xue@gmail.com>
2018-02-12 23:03:17 -0500

    get feature matrix

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -1,5 +1,6 @@
+import numpy as np
 
-def extract_feature_vector(vocab_list, training_set):
+def extract_feature_matrix(vocab_list, training_set):
     vocab_to_index = get_vocab_to_index(vocab_list) 
     indicies = [words_to_indices(email_obj, vocab_to_index) for email_obj in training_set]
     feature_matrix = get_feature_matrix(indicies, vocab_list) 
@@ -19,4 +20,14 @@ def words_to_indices(email_obj, vocab_to_index):
     email_obj['contents'] = list(filter(lambda x: x >= 0, indicies))
     return email_obj
 
+"""
+Create an m x n matrix where each row is the feature vector of a training example,
+there are m training examples, and each column is a specific feature
+"""
 def get_feature_matrix(indicies, vocab_list):
+    feature_matrix = np.zeros((len(indicies), len(vocab_list)), dtype=int)
+    for i in range(len(indicies)):
+        word_indicies = indicies[i].get('contents')
+        for j in range(len(word_indicies)):
+            feature_matrix[i,word_indicies[j]] = 1
+    return feature_matrix
--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from load_data import load_data
 from vocab_list import get_vocab_list
-from feature_extraction import get_feature_vector
+from feature_extraction import extract_feature_matrix
 
 import vocab_list
 
 def main():
     training_set = load_data()
     vocab_list = get_vocab_list(training_set)
-    word_indicies = get_feature_vector(vocab_list, training_set)
+    X = extract_feature_matrix(vocab_list, training_set)
+    print(X)
 
 if __name__ == '__main__': main()
